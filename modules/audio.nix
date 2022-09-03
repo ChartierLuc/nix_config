afaeabff9db8b0fcdab0c7e753a43370816b0350
Luc <luc@distorted.audio>
2022-09-03 05:36:22 -0400

    diabled pulse audio

--- modules/audio.nix
+++ modules/audio.nix
@@ -3,6 +3,7 @@
   # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
   #sound.enable = false;
   hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = false;
   # rtkit is optional but recommended
   security.rtkit.enable = true;
   services.pipewire = {
