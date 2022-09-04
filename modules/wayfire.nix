ba427742c4ea10c7da25f7afa4fed1109ed737b6
Luc <luc@distorted.audio>
2022-09-03 21:19:12 -0400

    renamed oled to oled_gnome

---
+++ modules/wayfire.nix
@@ -0,0 +1,24 @@
+{ config, builtins, lib, pkgs, nixpkgs-wayland, ... }:
+{
+config = {
+          nix = {
+            # add binary caches
+            binaryCachePublicKeys = [
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+              # ...
+            ];
+            binaryCaches = [
+              "https://cache.nixos.org"
+              "https://nixpkgs-wayland.cachix.org"
+              # ...
+            ];
+          };
+  # use it as an overlay
+  nixpkgs.overlays = [ nixpkgs-wayland.overlay ];
+
+environment.systemPackages = [
+    pkgs.wayfire
+  ];
+
+}
