e10ac91b42b1d6bf1ff62ffd4e1b2c42e39a486f
Luc <luc@distorted.audio>
2022-09-25 23:53:43 -0400

    needed to commit

--- flake.nix
+++ flake.nix
@@ -2,34 +2,33 @@
 description = "Luc's nix configuration";
 
 inputs = {
-            nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-            unstable.follows = "nixpkgs";
+  nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+  unstable.follows = "nixpkgs";
   
-            nixpkgs-wayland  = {
-                                  url = "github:nix-community/nixpkgs-wayland"; 
-                                };
+  nixpkgs-wayland  = {
+    url = "github:nix-community/nixpkgs-wayland"; 
+  };
 
-            # # home-manager pins nixpkgs to a specific version in its flake.
-            # # we want to make sure everything pins to the same version of nixpkgs to be more efficient
-            home-manager = {
-                              url = github:nix-community/home-manager;
-                              inputs.nixpkgs.follows = "nixpkgs";
-                            };
+  # # home-manager pins nixpkgs to a specific version in its flake.
+  # # we want to make sure everything pins to the same version of nixpkgs to be more efficient
+  home-manager = {
+    url = github:nix-community/home-manager;
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-            # # agenix allows me to store encrypted secrets in the repo just like git-crypt, except
-            # # it integrates with nix so I don't need to have world-readable secrets in the nix store.
-            # agenix = {
-            #             url = "github:ryantm/agenix";
-            #             inputs.nixpkgs.follows = "nixpkgs";
-            #           };
+  # # agenix allows me to store encrypted secrets in the repo just like git-crypt, except
+  # # it integrates with nix so I don't need to have world-readable secrets in the nix store.
+  # agenix = {
+  #             url = "github:ryantm/agenix";
+  #             inputs.nixpkgs.follows = "nixpkgs";
+  #           };
 
-            #TODO: separate each config into its own flake to avoid pulling unnecessary deps? or is nix smart enough
-            nixos-wsl = {
-                          url = "github:nix-community/NixOS-WSL";
-                          inputs.nixpkgs.follows = "nixpkgs";
-                        };
-
-          };
+  #TODO: separate each config into its own flake to avoid pulling unnecessary deps? or is nix smart enough
+  nixos-wsl = {
+    url = "github:nix-community/NixOS-WSL";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  };
 
 outputs = inputs@{self, nixpkgs, home-manager,  ...}:
 
@@ -38,54 +37,54 @@ let
   system = "x86_64-linux";
   
   pkgs = import nixpkgs {
-                          config.allowUnfree = true;
-                          # TODO: Look at overlays
-                          overlays = [
-                          ];
-                        };
+    config.allowUnfree = true;
+    # TODO: Look at overlays
+    overlays = [
+               ];
+  };
+
 in {
   # TODO: Look at overlays
   inputs.nixpkgs.overlays = [
     # import ./overlays/default.nix
   ]; 
   nixosConfigurations = { 
-                          miBook = inputs.nixpkgs.lib.nixosSystem { 
-                            system = "x86_64-linux";
-                              modules = [
-                                home-manager.nixosModules.home-manager {
-                                home-manager.useGlobalPkgs = true; # instead of having its own private nixpkgs
-                                home-manager.useUserPackages = true; # install to /etc/profiles instead of ~/.nix-profile
-                                home-manager.extraSpecialArgs = {
-                                  inherit user; # pass user to modules in conf (home.nix or whatever)
-                                  configName = "miBook";
-                                };
+    miBook = inputs.nixpkgs.lib.nixosSystem { 
+    system = "x86_64-linux";
+    modules = [
+    home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true; # instead of having its own private nixpkgs
+      home-manager.useUserPackages = true; # install to /etc/profiles instead of ~/.nix-profile
+      home-manager.extraSpecialArgs = {
+        inherit user; # pass user to modules in conf (home.nix or whatever)
+        configName = "miBook";
+      };
 
-                          home-manager.users.luc = import ./home-manager/luc.nix;
-                                }
-                                # Hardware configuration
-                                ./hosts/miBook/host.nix
+      home-manager.users.luc = import ./home-manager/luc.nix;
+    }
+    # Hardware configuration
+    ./hosts/miBook/host.nix
 
-                                # Device is a personal laptop
-                                #./config/laptop.nix
-                                ./config/base-desktop.nix
-                                ./config/personal.nix
-                                ./config/cli.nix
-                                
-                                # Give access to network filestore
-                              #  ./config/file_access.nix
-                              
-                              # # Use X11 Gnome
-                                ./config/xorg.nix
-                                ./config/oled_gnome.nix
+    # Device is a personal laptop
+    ./config/base-desktop.nix
+    ./config/personal.nix
+    ./config/cli.nix
+    
+    # Give access to network filestore
+    #  ./config/file_access.nix
+  
+    # # Use X11 Gnome
+    ./config/xorg.nix
+    ./config/oled_gnome.nix
 
-                              # Use Wayland Wayfire
-                              #  ./module/wayfire.nix
+    # Use Wayland Wayfire
+    #  ./module/wayfire.nix
 
-                                # Use pipewire
-                                ./module/audio.nix
-                              ];
-                              specialArgs = { inherit inputs; };
-                            };
+    # Use pipewire
+    ./module/audio.nix
+  ];
+  specialArgs = { inherit inputs; };
+};
 
                             G7 = inputs.nixpkgs.lib.nixosSystem { 
                             system = "x86_64-linux";
@@ -100,16 +99,15 @@ in {
                           home-manager.users.luc = import ./home-manager/luc.nix;
                                 }
                                 # Hardware configuration
-                                ./hosts/miBook/host.nix
+                                ./hosts/G7/host.nix
 
                                 # Device is a personal laptop
-                              # ./config/laptop.nix
                                 ./config/base-desktop.nix
                                 ./config/personal.nix
                                 ./config/cli.nix
                                 
                                 # Give access to network filestore
-                              #  ./config/file_access.nix
+                                #  ./config/file_access.nix
                               
                               # # Use X11 Gnome
                                 ./config/xorg.nix
