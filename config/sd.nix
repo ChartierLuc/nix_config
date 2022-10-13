a365756972798e4e52694b9aba41af641e6bf7d3
luc <luc@distorted.audio>
2022-10-13 16:36:47 -0400

    added reverse proxy to sd.nix

--- config/sd.nix
+++ config/sd.nix
@@ -1,14 +1,27 @@
 { config, pkgs, lib, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 7860 ];
+    allowedTCPPorts = [ 80 443 ];
   };
+
+  security.acme = {
+    acceptTerms = true;
+    email = "admin@distored.audio";
+  };
+
+  services.nginx.virtualHosts = let
+    SSL = {
+      enableACME = true;
+      forceSSL = true;
+    }; in {
+      "frieza.bee.hive" = (SSL // {
+        locations."/sd".proxyPass = "http://127.0.0.1:7862/";
+
+        serverAliases = [
+          "frieza.bee.hive"
+        ];
+      });
+    };
 }
