b89f0da77a89727f858d58847af8d2e718100936
Luc Chartier <luc@Lucs-Air.bee.hive>
2022-12-09 22:49:16 -0500

    added synergy to air config

--- hosts/air/host.nix
+++ hosts/air/host.nix
@@ -1,10 +1,20 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+
+  environment.systemPackages = 
+  [
+    pkgs.vim
+    pkgs.tmux
+    pkgs.git
+    pkgs.synergy
+  ];
+
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
-  
+  #services.nix-daemon.package = pkgs.nixFlakes;
+  nix.package = pkgs.nix;
+
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
   programs.zsh.enable = true;
@@ -12,11 +22,11 @@
 
   homebrew = {
     enable = true;
-    autoUpdate = true;
+    onActivation.autoUpdate = true;
 
     casks = [
-      "libsodium"
-      "openssl"
+      # "libsodium"
+      # "cmake"
     ];
   };
 }
